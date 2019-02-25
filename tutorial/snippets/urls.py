eab3211d288fe5f339abf59fab34ebf0a4e79cd3
akashgiricse <akashgiricse@gmail.com>
2019-02-25 22:46:36 +0530

    Install rest_framework_swagger for documenting api

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,8 +2,10 @@ from rest_framework.schemas import get_schema_view
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_schema_view(title='Pastebin API')
+swagger_schema_view = get_swagger_view(title='Pastebin API')
 
 
 # create a router and register our viewset with it
@@ -14,6 +16,7 @@ router.register(r'users', views.UserViewSet)
 # the API URLs are now determined automatically by the routers
 urlpatterns = [
     path('schema/', schema_view),
+    path('swagger-docs', swagger_schema_view),
     path('', include(router.urls)),
 
 ]
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'snippets.apps.SnippetsConfig',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
